79ed85ce55fbc15c069192c13e5ba4db7b99954d
amanichifwete-boop <amanichifwete@gmail.com>
2025-12-09 15:42:02 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+// Placeholder script for Ramadan Reminder automation.
+// This file will later fetch Google Sheet data and send WhatsApp templates.
+
+function main() {
+  console.log("Ramadan reminder automation placeholder is running...");
+}
+
+main();
